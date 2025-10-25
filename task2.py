3c3ffca44cfb1ea94a5ade0e17148929cca14283
iboy-x <abdulmoiz.adeel29@gmail.com>
2025-10-25 18:01:56 +0500

    Your commit message here

--- task2.py
+++
@@ -1,9 +0,0 @@
-'''
-Take your sibming age as input and out put 
-how old they will be when you turn 50
-'''
-
-sibling_age = int(input("Enter your sibling's age: "))
-your_age = int(input("Enter your age: "))
-age_difference = your_age - sibling_age
-print("Your sibling will be", sibling_age + (50 - your_age), "when you turn 50.")

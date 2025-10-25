f277ae637789ce0daa602d486fc6de8960be960d
iboy-x <abdulmoiz.adeel29@gmail.com>
2025-10-25 11:47:13 +0500

    first commit

---
+++ basics.py
@@ -0,0 +1,16 @@
+#variableName = value
+# = assignment 
+# hastag is for comments 
+
+#DECLARE variable : Data type 
+#Decalare nu,m: INTERGER
+num = 5 
+
+
+#print() => can take multiple paramenters. By default, outputs a new line at the end 
+print(num) 
+print("Hello World") 
+print("My number is:", num) 
+
+ans = num + 2 
+print("My new number is:", ans)
